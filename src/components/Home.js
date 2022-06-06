0c1f3f0afecfdd4c89f8e8a75c8d8f6af0048e55
Segun Ajibola <ajbl.segun@gmail.com>
2022-06-06 16:21:27 +0100

    first commit

--- src/components/Home.js
+++ src/components/Home.js
@@ -84,74 +84,75 @@ const Home = () => {
             className='my-2 h-52 w-full rounded-lg object-center shadow-xl sm:mt-8 sm:h-64 sm:object-cover lg:hidden'
           />
         </div>
+      </div>
+
+
         
-          <div className='grid bg-gray-100 pt-10 dark:bg-gray-700 grid-cols-2 w-full pb-10'>
-              <div className='relative ml-5 mx-auto w-[42rem] px-4 py-4'>
-                <h1 className='mt-2 text-xl font-bold text-gray-900 dark:text-white sm:mt-4 sm:text-4xl lg:mt-4 lg:text-5xl'>Social proofs are cool</h1>
-                <p className='mt-8 text-gray-600 dark:text-white sm:text-xl'>
-                Here's how -- adipisicing elit. Ratione autem ipsum voluptatem, fuga vel laborum eius laudantium iste culpa sed blanditiis, et suscipit velit ea quisquam. Distinctio in dolores placeat. Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum, ad, atque ut impedit modi soluta assumenda perspiciatis consectetur blanditiis deserunt inventore rerum delectus. Soluta accusamus aliquam tempore praesentium ipsam vitae. adipisicing elit elit. Ipsum, ad, atque ut impedit modi soluta assumenda perspiciatis consectetur blanditiis deserunt inventore rerum delectus. Soluta accusamus aliquam tempore praesentium ipsam vitae.
-                </p>
-              </div>
-              <div className='mx-auto ml-[22rem] px-4 sm:max-w-xl lg:max-w-full xl:mr-0 2xl:col-span-2'>
-                <Swiper
-                spaceBetween={30}
-                centeredSlides={true}
-                autoplay={{
-                    delay: 2500,
-                    disableOnInteraction: false,
-                }}
-                pagination={{
-                    clickable: true,
-                }}
-                navigation={true}
-                modules={[Autoplay, Pagination, Navigation]}
-                className="relative w-[24.5rem] sm:w-[30rem] m-auto md:w-[38rem]"
-                >
-                    <SwiperSlide className="md:mt-1 p-2">
-                        <figure className="bg-gray-700 dark:bg-gray-300 grid rounded-xl p-3 shadow-lg shadow-gray-700">
-                            <img className="w-32 h-32 rounded-full mx-auto" src="./" alt="" />
-                            <div className="pt-4 text-center space-y-3">
-                            <blockquote className='mx-auto'>
-                                <p className="text-lg font-semibold text-gray-300 mx-4 dark:text-gray-900">
-                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt ullam harum impedit in, alias tempora aliquam provident ipsa voluptas unde consequatur, nihil minima. Asperiores vero minus magnam atque a suscipit.
-                                </p>
-                            </blockquote>
-                            <figcaption className="font-medium text-gray-500 dark:text-gray-800">
-                                <div className="">
-                                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                                </div>
-                                <div className="mb-2">
-                                Lorem ip
-                                </div>
-                            </figcaption>
-                            </div>
-                        </figure>
-                    </SwiperSlide>
-                    <SwiperSlide className="md:mt-1 p-2">
-                        <figure className="bg-gray-700 dark:bg-gray-300 grid rounded-xl p-3 shadow-lg shadow-gray-700">
-                            <img className="w-32 h-[15em] rounded-full mx-auto" src="./" alt="" />
-                            <div className="pt-4 text-center space-y-3">
-                            <blockquote className='mx-auto'>
-                                <p className="text-lg font-semibold text-gray-300 mx-4 dark:text-gray-900">
-                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt ullam harum impedit in, alias tempora aliquam provident ipsa voluptas unde consequatur, nihil minima. Asperiores vero minus magnam atque a suscipit.
-                                </p>
-                            </blockquote>
-                            <figcaption className="font-medium text-gray-500 dark:text-gray-800">
-                                <div className="">
-                                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                                </div>
-                                <div className="mb-2">
-                                Lorem ip
-                                </div>
-                            </figcaption>
-                            </div>
-                        </figure>
-                    </SwiperSlide>
-                </Swiper>
-              </div>
-          </div>
-          
+      <div className='grid bg-gray-100 pt-10 dark:bg-gray-700 grid-cols-2 w-full pb-10'>
+        <div className='relative ml-5 mx-auto w-[42rem] px-4 py-4'>
+          <h1 className='mt-2 text-xl font-bold text-gray-900 dark:text-white sm:mt-4 sm:text-4xl lg:mt-4 lg:text-5xl'>Social proofs are cool</h1>
+          <p className='mt-8 text-gray-600 dark:text-white sm:text-xl'>
+            Here's how -- adipisicing elit. Ratione autem ipsum voluptatem, fuga vel laborum eius laudantium iste culpa sed blanditiis, et suscipit velit ea quisquam. Distinctio in dolores placeat. Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum, ad, atque ut impedit modi soluta assumenda perspiciatis consectetur blanditiis deserunt inventore rerum delectus. Soluta accusamus aliquam tempore praesentium ipsam vitae. adipisicing elit elit. Ipsum, ad, atque ut impedit modi soluta assumenda perspiciatis consectetur blanditiis deserunt inventore rerum delectus. Soluta accusamus aliquam tempore praesentium ipsam vitae.
+          </p>
         </div>
+        <div className='mx-auto ml-[2rem] px-4 sm:max-w-xl lg:max-w-full xl:mr-0 2xl:col-span-2'>
+          <Swiper
+          spaceBetween={30}
+          centeredSlides={true}
+          autoplay={{
+              delay: 2500,
+              disableOnInteraction: false,
+          }}
+          pagination={{
+              clickable: true,
+          }}
+          navigation={true}
+          modules={[Autoplay, Pagination, Navigation]}
+          className="relative w-[24.5rem] sm:w-[30rem] m-auto md:w-[38rem]"
+          >
+            <SwiperSlide className="md:mt-1 p-2">
+                <figure className="bg-gray-700 dark:bg-gray-300 grid rounded-xl p-3 shadow-lg shadow-gray-700">
+                    <img className="w-32 h-32 rounded-full mx-auto" src="./" alt="" />
+                    <div className="pt-4 text-center space-y-3">
+                    <blockquote className='mx-auto'>
+                        <p className="text-lg font-semibold text-gray-300 mx-4 dark:text-gray-900">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt ullam harum impedit in, alias tempora aliquam provident ipsa voluptas unde consequatur, nihil minima. Asperiores vero minus magnam atque a suscipit.
+                        </p>
+                    </blockquote>
+                    <figcaption className="font-medium text-gray-500 dark:text-gray-800">
+                        <div className="">
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                        </div>
+                        <div className="mb-2">
+                        Lorem ip
+                        </div>
+                    </figcaption>
+                    </div>
+                </figure>
+            </SwiperSlide>
+            <SwiperSlide className="md:mt-1 p-2">
+                <figure className="bg-gray-700 dark:bg-gray-300 grid rounded-xl p-3 shadow-lg shadow-gray-700">
+                    <img className="w-32 h-[15em] rounded-full mx-auto" src="./" alt="" />
+                    <div className="pt-4 text-center space-y-3">
+                    <blockquote className='mx-auto'>
+                        <p className="text-lg font-semibold text-gray-300 mx-4 dark:text-gray-900">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt ullam harum impedit in, alias tempora aliquam provident ipsa voluptas unde consequatur, nihil minima. Asperiores vero minus magnam atque a suscipit.
+                        </p>
+                    </blockquote>
+                    <figcaption className="font-medium text-gray-500 dark:text-gray-800">
+                        <div className="">
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                        </div>
+                        <div className="mb-2">
+                        Lorem ip
+                        </div>
+                    </figcaption>
+                    </div>
+                </figure>
+            </SwiperSlide>
+          </Swiper>
+        </div>
+      </div>
     </>
   );
 };
