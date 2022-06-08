d54de4b30cbaef7764aa4670f67fe7817a1ce530
Segun Ajibola <ajbl.segun@gmail.com>
2022-06-08 17:26:35 +0100

    first commit

--- src/components/Register.js
+++ src/components/Register.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 
 const Register = () => {
     // const initialValues = {} or set {} in formValues
-    const initialValues = {username: "", email: "", password: ""}
+    const initialValues = {username: "", email: "", password: "", confirmpassword: ""}
     const [formValues, setFormValues] = useState(initialValues)
     const [formErrors, setFormErrors] = useState({})
     const [isSubmit, setIsSubmit] = useState(false)
@@ -46,12 +46,20 @@ const Register = () => {
         } else if (!regex.test(values.email)) {
             errors.email = "This is not a valid email format!";
         }
+        if (!values.phonenumber) {
+            errors.phonenumber = "Phone number is required!"
+        } else if (isNaN(values.phonenumber)) {
+            errors.phonenumber = "Phone number should be a number"
+        }
         if (!values.password) {
             errors.password = "Password is required!"
         } else if (values.password.length < 4) {
             errors.password = "Password must be more than 4 characters.";
         } else if (values.password.length > 10) {
             errors.password = "Password must be not be more than 10 characters.";
+        }
+        if (values.password !== values.confirmpassword) {
+            errors.confirmpassword = "Password do not match!"
         }
         return errors;
     }
@@ -66,28 +74,18 @@ const Register = () => {
             <div>Signed in successfully</div>) : (
                 <pre>{JSON.stringify(formValues, undefined, 2)}</pre>
             )} */}
-            <p className='text-inactive mt-3'>Enter details to sign up</p>
+            <p className='text-inactive mt-3'>Enter your details to sign up</p>
             <form method='POST' action="" onSubmit={handleSubmit} className='text-left flex flex-col max-w-[600px] w-full mx-auto my-5'>
-            <label className="my-2">First Name</label>
+                <label className="my-2">Full Name</label>
                 <input 
                     className='bg-[#ccd6f6] p-2' 
                     type="text"
-                    id="firstname" 
-                    placeholder='First Name' 
-                    name='firstname' 
-                    value={ formValues.firstname }
+                    id="fullname" 
+                    placeholder='Full Name' 
+                    name='username' 
+                    value={ formValues.username }
                     onChange={handleChange} />
-                <p>{ formErrors.firstname }</p>
-                <label className="my-2">Last Name</label>
-                <input 
-                    className='bg-[#ccd6f6] p-2' 
-                    type="text"
-                    id="lastname" 
-                    placeholder='Last Name' 
-                    name='lastname' 
-                    value={ formValues.lastname }
-                    onChange={handleChange} />
-                <p>{ formErrors.lastname }</p>
+                <p>{ formErrors.username }</p>
                 <label className="my-2">E-mail</label>
                 <input 
                     className='mb-2 p-2 bg-[#ccd6f6]'
@@ -97,6 +95,15 @@ const Register = () => {
                     value={ formValues.email }
                     onChange={handleChange} />
                 <p>{ formErrors.email }</p>
+                <label className="my-2">Phone Number</label>
+                <input 
+                    className='mb-2 p-2 bg-[#ccd6f6]'
+                    type="tel"
+                    placeholder='Phone number'
+                    name='phonenumber'
+                    value={ formValues.phonenumber }
+                    onChange={handleChange} />
+                <p>{ formErrors.phonenumber }</p>
                 <label className="my-2">Password</label>
                 <input 
                     className='mb-2 p-2 bg-[#ccd6f6]' 
@@ -106,19 +113,21 @@ const Register = () => {
                     value={ formValues.password }
                     onChange={handleChange} />
                 <p>{ formErrors.password }</p>
-                {/* <p>{ confirm }</p> */}
+                <label className="my-2">Confirm assword</label>
+                <input 
+                    className='mb-2 p-2 bg-[#ccd6f6]' 
+                    type="password" 
+                    placeholder='confirm password' 
+                    name='confirmpassword'
+                    value={ formValues.confirmpassword }
+                    onChange={handleChange} />
+                <p>{ formErrors.confirmpassword }</p>
                 
                 {Object.keys(formErrors).length === 0 && isSubmit ? (
                 <div>Signed in successfully</div>) : "" }
 
-                <button className='border-2 w-20 border-black bg-indigo-300 hover:bg-gray-700 hover:text-white hover:border-gray-600 py-2 px-4 rounded-lg mt-5 mx-auto flex items-center'>Login</button>
-            </form>
-            <div className='mb-10'>
-                <Link to='/' className='text-indigo-600 text-md'>
-                Forgot Password?
-            </Link>
-            </div>
-            
+                <button className='border-2 w-40 border-black bg-indigo-300 hover:bg-gray-700 hover:text-white hover:border-gray-600 py-2 px-4 rounded-lg mt-5 mx-auto flex items-center'>Create Account</button>
+            </form>            
         </div>
     </>
   );
